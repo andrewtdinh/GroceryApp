fd23c0c90f34a29126ad0540f556b25a46100a45
Andrew Dinh <andrewdinh74@yahoo.com>
2016-04-05 09:41:15 -0700

    Fix error about not able to find React

--- index.ios.js
+++ index.ios.js
@@ -3,26 +3,61 @@
  * https://github.com/facebook/react-native
  */
 const Firebase = require('firebase');
-const styles = require('./styles.js')
+const styles = require('./styles.js');
+const StatusBar = require('./components/StatusBar');
+const ActionButton = require('./components/ActionButton');
+const ListItem = require('./components/ListItem');
+
 
 import React, {
   AppRegistry,
+  ListView,
+  AlertIOS,
   Component,
   StyleSheet,
   Text,
   View
 } from 'react-native';
 
-class GroceryApp extends React.Component {
+// var { AppRegistry, StyleSheet, Text, View, ListView, AlertIOS } = React;
+
+class GroceryApp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      })
+    };
+  }
+  componentDidMount() {
+    this.setState({
+      dataSource: this.state.dataSource.cloneWithRows([{ title: 'Pizza' }])
+    })
+  }
+  _renderItem(item) {
+    return (
+      <ListItem item={item} onPress={() => {}} />
+    );
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>
-          I am in a container
-        </Text>
+
+        <StatusBar title="Grocery List" />
+
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          style={styles.listview}/>
+
+        <ActionButton title="Add" onPress={() => {}} />
+
       </View>
     );
   }
 }
+
 
 AppRegistry.registerComponent('GroceryApp', () => GroceryApp);
